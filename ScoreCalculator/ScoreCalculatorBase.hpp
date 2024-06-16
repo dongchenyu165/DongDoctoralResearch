ce82206749878bcae63d321884251c3e7ec24327
cookteam <tou@robo.lab.uec.ac.jp>
2024-06-17 04:18:20 +0900

    refactor(ScoreCalculatorBase): Rename some types

--- ScoreCalculator/ScoreCalculatorBase.hpp
+++ ScoreCalculator/ScoreCalculatorBase.hpp
@@ -8,17 +8,17 @@
 
 #include <Utilities/JSON_Helper/StructSerializer.hpp>
 
-template<typename DataType, typename ConfigDataType, int ScoreComponentCount>
+template<typename InputDataType, typename ConfigObjType, int ScoreComponentCount>
 class TScoreCalculatorBase
 {
-	using Self = TScoreCalculatorBase<DataType, ConfigDataType, ScoreComponentCount>;
+	using Self = TScoreCalculatorBase<InputDataType, ConfigObjType, ScoreComponentCount>;
 
 public:
-	using DataScoreType     = std::pair<double, DataType>;
+	using DataScoreType     = std::pair<double /* Score */, InputDataType /* Input Data Obj*/>;
 	using DataScoreListType = std::vector<DataScoreType>;
 
-	using ScoreDataRowType      = Eigen::Matrix<float, 1, ScoreComponentCount>;
-	using ScoreDataType         = Eigen::Matrix<float, -1, ScoreComponentCount>;
+	using ScoreMatType         = Eigen::Matrix<float, -1, ScoreComponentCount>;
+	using ScoreMatRowType      = Eigen::Matrix<float, 1, ScoreComponentCount>;
 	using ScoreWeightVectorType = Eigen::Matrix<float, ScoreComponentCount, 1>;
 
 	using StrategiesType = std::map<std::string, DataScoreListType, std::less<std::string>>;
@@ -40,25 +40,16 @@ protected:
 	 * @param InData The data for calculating score.
 	 * @return double Score.
 	 */
-	virtual double CalcScore(const DataType& InData) = 0;
+	virtual double CalcRawScore(const InputDataType& InData) = 0;
 
 
 	void SortByScore() { std::stable_sort(DataScoreList.begin(), DataScoreList.end(), &Self::SortPredicate); }
 	static bool SortPredicate(const DataScoreType& InA, const DataScoreType& InB) { return InA.first > InB.first; }
 
 public:
-	virtual void GetStrategies(StrategiesType& OutStrategies, bool bDebugMode = false) = 0;
-
-	void SetDataSize(size_t InDataSize)
+	void AddCalculatingData(const InputDataType& InData)
 	{
-		DataScoreList.reserve(InDataSize);
-		ScoreRawData.resize(InDataSize, ScoreComponentCount);
-		CurrentDataIdx = 0;
-	}
-
-	void AddData(const DataType& InData)
-	{
-		DataScoreList.push_back(std::make_pair(CalcScore(InData), InData));
+		DataScoreList.push_back({CalcRawScore(InData), InData});
 		CurrentDataIdx++;
 	}
 
@@ -66,9 +57,9 @@ public:
 
 protected:
 	DataScoreListType DataScoreList;
-	ConfigDataType ConfigData;
+	ConfigObjType ConfigData;
 
-	ScoreDataType ScoreRawData;
+	ScoreMatType ScoreRawData;
 	ScoreWeightVectorType ScoreWeight;
 
 	size_t CurrentDataIdx = 0;
