3f42926705b6ae8d3e56d68e623eef5f16346dbf
cookteam <tou@robo.lab.uec.ac.jp>
2024-06-17 21:34:28 +0900

    style(ScoreCalculatorBase): Re-Formate code.

--- ScoreCalculator/ScoreCalculatorBase.hpp
+++ ScoreCalculator/ScoreCalculatorBase.hpp
@@ -9,19 +9,20 @@
 #include "DataTypes/PointSetData.hpp"
 #include <Utilities/JSON_Helper/StructSerializer.hpp>
 
-
 /**
  * @brief Basic class for calculating score.
  Input [InputDataType] datas by [AddCalculatingData] function.
-	The raw score of added data'll calculated simutaneously.
+    The raw score of added data'll calculated simutaneously.
 
  After all datas added, call [CalcFinalScore] function to calculate the final score,
-	and also sort datas by the final score in descending order.
- * 
+    and also sort datas by the final score in descending order.
+ *
  * @tparam InputDataType Input data's type to calculate the raw score. like [position normal ...] of a point-set
- * @tparam ConfigDataType Config object type of this object, mainly contains [Weight Vector] or other config parameter for calculating.
- * @tparam ScoreComponentCount How many score component to calculate the final score of the input data. Exp: 
- Final Score = ScoreComp_1 * Weight_1 + ScoreComp_2 * Weight_2 + ... + ScoreComp_n * Weight_n  (n = [ScoreComponentCount])
+ * @tparam ConfigDataType Config object type of this object, mainly contains [Weight Vector] or other config parameter
+ for calculating.
+ * @tparam ScoreComponentCount How many score component to calculate the final score of the input data. Exp:
+ Final Score = ScoreComp_1 * Weight_1 + ScoreComp_2 * Weight_2 + ... + ScoreComp_n * Weight_n  (n =
+ [ScoreComponentCount])
  */
 template<typename InputDataType, typename ConfigObjType, int ScoreComponentCount>
 class TScoreCalculatorBase
@@ -33,18 +34,19 @@ class TScoreCalculatorBase
 
 	// A matrix type to storage the score data with size (Pre-Allocated DataSize, ComponentSize)
 	// 		[Pre-Allocated DataSize]: Dynamic size. Set by the construct function's [InDataSize] argument.
-	using ScoreMatType         = Eigen::Matrix<float, -1, ScoreComponentCount>;
+	using ScoreMatType          = Eigen::Matrix<float, -1, ScoreComponentCount>;
 	using ScoreWeightVectorType = Eigen::Matrix<float, ScoreComponentCount, 1>;
 
 public:
-
-	TScoreCalculatorBase(const json& InConfigJsonObj, const size_t InDataSize, EvaluationStaticData& InStaticData) : StaticData(InStaticData) { 
+	TScoreCalculatorBase(const json& InConfigJsonObj, const size_t InDataSize, EvaluationStaticData& InStaticData)
+		: StaticData(InStaticData)
+	{
 		// std::vector init;
-		DataScoreList.resize(InDataSize, {INTMAX_MIN, InputDataType()});
+		DataScoreList.resize(InDataSize, { INTMAX_MIN, InputDataType() });
 
 		// Eigen matrix init.
 		ScoreRawData.resize(InDataSize, ScoreComponentCount);
-		ScoreRawData.setConstant(-INFINITY);  // Init to min of the float.
+		ScoreRawData.setConstant(-INFINITY); // Init to min of the float.
 	}
 
 protected:
@@ -53,53 +55,57 @@ protected:
 	 MUST be implementated in child class.
 	 MEANS: YOU MUST implementate:
 	 1. Calculate each score component:
-	 	ScoreComp_1 = CalcScoreComp_1(InData.data1, StaticData.data2);
-	 	ScoreComp_2 = CalcScoreComp_2(InData.data1, InData.data2, InData.data4);
-		...
-	 	ScoreComp_n = CalcScoreComp_n(InData.data3, InData.data4, StaticData.data3);
+	    ScoreComp_1 = CalcScoreComp_1(InData.data1, StaticData.data2);
+	    ScoreComp_2 = CalcScoreComp_2(InData.data1, InData.data2, InData.data4);
+	    ...
+	    ScoreComp_n = CalcScoreComp_n(InData.data3, InData.data4, StaticData.data3);
 	 2. Fill the matrix row the row index [CurrentDataIdx] with each score-component:
-	 	ScoreRawData[CurrentDataIdx] = [ScoreComp_1, ScoreComp_2, ... , ScoreComp_n]
-	 * 
+	    ScoreRawData[CurrentDataIdx] = [ScoreComp_1, ScoreComp_2, ... , ScoreComp_n]
+	 *
 	 * @param InData The data for calculating score.
 	 * @return double Score.
 	 */
 	virtual double CalcRawScore(const InputDataType& InData) = 0;
 
-/**
- * @brief Sort the [DataScoreList] by their score in descending order.
- * 
- */
+	/**
+	 * @brief Sort the [DataScoreList] by their score in descending order.
+	 *
+	 */
 	void SortByScore() { std::stable_sort(DataScoreList.begin(), DataScoreList.end(), &Self::SortPredicate); }
+
 	/**
 	 * @brief A function to decide data A is LARGER than B.
-	 * 
+	 *
 	 * @param InA data A
 	 * @param InB data B
 	 * @return true A is LARGER than B.
 	 * @return false A is SMALLER than B.
 	 */
-	static bool SortPredicate(const DataScorePairType& InA, const DataScorePairType& InB) { return InA.first > InB.first; }
+	static bool SortPredicate(const DataScorePairType& InA, const DataScorePairType& InB)
+	{
+		return InA.first > InB.first;
+	}
 
 public:
-/**
- * @brief Mainly used function. 
- * 			Calculate score of the input [InputDataType] data, 
- * 			and add it to list for further score-based-sorting.
- * 
- * @param InData [InputDataType] usually be [] type, for score calculating.
- */
+	/**
+	 * @brief Mainly used function.
+	 * 			Calculate score of the input [InputDataType] data,
+	 * 			and add it to list for further score-based-sorting.
+	 *
+	 * @param InData [InputDataType] usually be [] type, for score calculating.
+	 */
 	void AddCalculatingData(const InputDataType& InData)
 	{
-		DataScoreList.push_back({CalcRawScore(InData), InData});
+		DataScoreList.push_back({ CalcRawScore(InData), InData });
 		CurrentDataIdx++;
 	}
 
-/**
- * @brief Mainly used function.
-			Normalize each score component at their own.
-			Also apply the weight of each component.
-			Should called after all calculating datas was added.
- */
+	/**
+	 * @brief Mainly used function.
+	            Normalize each score component at their own.
+	            Also apply the weight of each component.
+	            Should called after all calculating datas was added.
+	 */
 	virtual void CalcFinalScore()
 	{
 		// Resize the score matrix to the fit the real size of datas.
@@ -134,7 +140,7 @@ protected:
 
 	// A matrix to storage the score data with size (Pre-Allocated DataSize, ComponentSize).
 	// 		[Pre-Allocated DataSize] set by the construct function's [InDataSize] argument.
-	// 			will shrink to the real size of datas at the start of [CalcFinalScore] function. 
+	// 			will shrink to the real size of datas at the start of [CalcFinalScore] function.
 	ScoreMatType ScoreRawData;
 	ScoreWeightVectorType ScoreWeight;
 
