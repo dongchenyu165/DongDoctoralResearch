e336a70e713e18a68678c81e2cebd63ded0fd175
cookteam <tou@robo.lab.uec.ac.jp>
2024-11-05 16:57:04 +0900

    feat(FilterGEO): Add filter class skeleton.

--- 2_FilterByGeo/GeometryFilter.cpp
+++ 2_FilterByGeo/GeometryFilter.cpp
@@ -1,9 +1,10 @@
 #include "GeometryFilter.hpp"
+#include "DataTypes/PointSetData.hpp"
+#include "GlobalTypes.hpp"
+#include "ScoreCalculator/ScoreCalculatorBase.hpp"
 
-
-Types::SearchSpace GeometryFilter(const Types::SearchSpace& InInitSearchSpace,
-	Types::Vec3 InCuttingPlaneNormal,
-	Types::TrajectoryNode& InTrajectoryInfo) 
+Types::SearchSpace GeometryFilter(const Types::SearchSpace& InInitSearchSpace)
 {
+	// TScoreCalculatorBase<int, int, 3> CalObj(json(), 20, EvaluationStaticData());
 	return Types::SearchSpace();
 }
--- 2_FilterByGeo/GeometryFilter.hpp
+++ 2_FilterByGeo/GeometryFilter.hpp
@@ -2,13 +2,12 @@
 #ifndef CCB4DCF3_818C_40AB_B035_D620866EE30D
 #define CCB4DCF3_818C_40AB_B035_D620866EE30D
 
+#include "DataTypes/PointSetData.hpp"
 #include "GlobalTypes.hpp"
 
-Types::SearchSpace GeometryFilter(const Types::SearchSpace& InInitSearchSpace,
-	Types::Vec3 InCuttingPlaneNormal,
-	Types::TrajectoryNode& InTrajectoryInfo);
-// {
-// 	return Types::SearchSpace();
-// }
+// Types::SearchSpace GeometryFilter(const Types::SearchSpace& InInitSearchSpace,
+	// Types::Vec3 InCuttingPlaneNormal,
+	// Types::TrajectoryNode& InTrajectoryInfo);
+Types::SearchSpace GeometryFilter(const Types::SearchSpace& InInitSearchSpace, EvaluationStaticData& InStaticData);
 
 #endif /* CCB4DCF3_818C_40AB_B035_D620866EE30D */
