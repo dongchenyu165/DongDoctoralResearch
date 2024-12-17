4fe671f08c5a16a945b120bcd8199cd7d0ed9be5
cookteam <tou@robo.lab.uec.ac.jp>
2024-12-17 22:29:54 +0900

    feat(Type): Add CalculateVelocity function to compute velocities for trajectory nodes

--- DataTypes/KnifeTrajectoryNode.hpp
+++ DataTypes/KnifeTrajectoryNode.hpp
@@ -3,6 +3,7 @@
 
 
 #include <Eigen/Core>
+#include <vector>
 
 /**
  * @brief 
@@ -35,5 +36,19 @@ public:
 	Eigen::Matrix<Scalar, 4, 4> Pose;
 	Eigen::Matrix<Scalar, 3, 1> Velocity;
 };
+
+template<typename Scalar>
+static void CalculateVelocity(std::vector<TKnifeTrajectoryNode<Scalar>>& InOutTrajectory, const std::vector<Scalar>& InScalarVelocity, const Eigen::Matrix<Scalar, 3, 1>& InEndVelocity = Eigen::Matrix<Scalar, 3, 1>::Zero())
+{
+	for (size_t i = 0; i < InOutTrajectory.size() - 1; i++)
+	{
+		const auto& CurrentPose = InOutTrajectory[i].Pose.template block<3, 1>(0, 3);
+		const auto& NextPose = InOutTrajectory[i + 1].Pose.template block<3, 1>(0, 3);
+		InOutTrajectory[i].Velocity = (NextPose - CurrentPose).normalized() * InScalarVelocity[i];
+	}
+
+	// The last node's velocity is the same as the previous node.
+	InOutTrajectory[InOutTrajectory.size() - 1].Velocity = InOutTrajectory[InOutTrajectory.size() - 2].Velocity;
+}
 
 #endif /* B55A87DF_08A1_4F3E_B3C2_D183A2E38E6F */
